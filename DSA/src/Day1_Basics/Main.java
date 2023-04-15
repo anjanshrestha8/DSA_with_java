4450a4365a3939eb42ead6564257943430b6b223
Anjan Shrestha <anjanshrestha@Anjans-MacBook-Air.local>
2023-04-15 10:08:01 +0545

    added dsa

--- DSA/src/Day1_Basics/Main.java
+++ DSA/src/Day1_Basics/Main.java
@@ -2,6 +2,6 @@ package Day1_Basics;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("hello world");
+        System.out.println("hello worldw");
     }
 }
